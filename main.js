8a99d71d3b9cbd73c75db2fc4fd4488f35827874
spencerbandtel <46567079+spencerbandtel@users.noreply.github.com>
2019-04-01 14:04:26 -0400

    Doesn't seem to work

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let pos = new Vector(60,60);
+let vel = new Vector(.5,.2);
+let radius = 50;
+
 function init() {
-    alert("hello World!");
+    pos.print();
     update();
 }
 
 function update() {
+    c.clearRect(0,0,canvas.width,canvas.height);
+
+    pos.addVector(vel);
+    pos.scale(radius);
+
+    c.beginPath();
+    c.arc(pos.x,pos.y,radius,0,Math.PI*2,false);
+    c.closePath();
+    c.stroke();
 
     requestAnimationFrame(update);
 }
--- vector.js
+++ vector.js
@@ -1,7 +1,35 @@
 class Vector {
-    
-    constructor() {
 
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addVector(vec){
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    // For hw
+    subVector(vec){
+        // subtract instead of addvector
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
+    }
+
+    scale(radius){
+        // change size of vector
+        this.radius += radius;
+        return this;
+    }
+
+    toString() {
+        return '<'+this.x+','+this.y+'>'
+    }
+    print(){
+        console.log(this.toString());
     }
 
 }
